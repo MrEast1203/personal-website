e9e3ba67119b604d4f4f2df5b9a6dadd0b888c2d
Kan <mreast1203@gmail.com>
2024-03-23 17:57:19 +0800

    add basenaame

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ChatPage from "./pages/ChatPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <Router>
+    <Router basename="/personal-website">
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
