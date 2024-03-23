7c8f7e50d4c1c6b31416dcc5dbdc16edf1ad7dc3
Kan <mreast1203@gmail.com>
2024-03-23 17:51:46 +0800

    add 404 handle

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import ChatPage from "./pages/ChatPage";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Router>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,26 +7,22 @@ export default function Header() {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global">
         <a
-          href="/#"
+          href="/"
           className="text-sm font-semibold leading-6 text-gray-900 mr-16">
           網絡攻防實習
         </a>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <a
-            href="/#"
-            className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
             About
           </a>
           <a
-            href="/#/chat"
+            href="/chat"
             className="text-sm font-semibold leading-6 text-gray-900">
             Chat
           </a>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a
-            href="/#"
-            className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
             Log in <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
