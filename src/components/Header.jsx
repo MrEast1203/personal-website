367dacad6aedad82c8b207baeafe9292201295d8
Kan <mreast1203@gmail.com>
2024-03-23 18:17:03 +0800

    a to Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Popover } from "@headlessui/react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -6,25 +7,29 @@ export default function Header() {
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global">
-        <a
-          href="/"
+        <Link
+          to="/"
           className="text-sm font-semibold leading-6 text-gray-900 mr-16">
           網絡攻防實習
-        </a>
+        </Link>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+          <Link
+            to="/"
+            className="text-sm font-semibold leading-6 text-gray-900">
             About
-          </a>
-          <a
-            href="/chat"
+          </Link>
+          <Link
+            to="/chat"
             className="text-sm font-semibold leading-6 text-gray-900">
             Chat
-          </a>
+          </Link>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+          <Link
+            to="/"
+            className="text-sm font-semibold leading-6 text-gray-900">
             Log in <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </nav>
     </header>
